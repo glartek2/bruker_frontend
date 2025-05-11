debd0d04db325f1d2521539db0b18ce39c53e446
szym-mie <szym.mie@gmail.com>
2025-05-11 21:04:35 +0200

    Sleep before generating api-schema, because nodejs is too fast for django

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
     plugins: [tailwindcss(), react()],
     server: {
         host: true,
-        port: 5173
+        port: 5173,
+        watch: {
+            usePolling: false
+        }
     }
 })
